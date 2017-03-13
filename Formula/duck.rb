06b4ff10cd4f57b29bfef94a52693f325fa2d487
Jenkins CI <noreply@cyberduck.io>
2017-03-13 17:51:20 +0100

    duck release-5-4-0

--- Formula/duck.rb
+++ Formula/duck.rb
@@ -2,8 +2,8 @@ class Duck < Formula
   desc "Command-line interface for Cyberduck (a multi-protocol file transfer tool)"
   homepage "https://duck.sh/"
   # check the changelog for the latest stable version: https://cyberduck.io/changelog/
-  url "https://dist.duck.sh/duck-src-4.7.3.18396.tar.gz"
-  sha256 "47e25f0a28393c388f37d319c9d51dd51eebdf15198ee48df3995af8a60bcc16"
+  url "https://dist.duck.sh//duck-src-5.4.0.23761.tar.gz"
+  sha256 "4a2048151dfaea66f439874c89a4ae67ce3e20cf169300707eb1042990c18086"
   head "https://svn.cyberduck.io/trunk/"
 
   bottle do
